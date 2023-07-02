069dd218ef121eb8f7859cbabb9a5b991690aca3
Julius Grönlund <97813052+juliusGronlund@users.noreply.github.com>
2023-07-02 13:45:18 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         System.out.println("This is my new program");
 
         int hej = 15;
+
+        //Följande kod kommer från github.
+        System.out.println("Hej, från github.");
     }
 }
